29e7841fa1644a7d2f4ccd8185b5011855833884
sebacris97 <counterlaplata@live.com>
2025-05-07 09:40:53 -0300

    test

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 from azlyrics_scraper import get_lyrics
 import requests
 from urllib.parse import unquote
@@ -17,8 +17,10 @@ async def root():
     return "main page"
 
 @app.get("/get-lyrics/{artist_name}/{song_name}")
-async def get_lyrics_view(artist_name: str,
-                          song_name: str):
+async def get_lyrics_view(artist_name:
+                          str = Path(..., description="Artist name to be rerieved."),
+                          song_name:
+                          str = Path(..., description="Song name to be rerieved."),):
     print(unquote(artist_name))
     print(unquote(song_name))
     lyrics = get_lyrics(
