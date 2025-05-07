4d4d8a138da2984586abe6d212e71e4e0473a646
sebacris97 <counterlaplata@live.com>
2025-05-07 09:13:32 -0300

    test

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from azlyrics_scraper import get_lyrics
 import requests
-from urllib.parse import unquote_plus
+from urllib.parse import unquote
 
 app = FastAPI()
 
@@ -12,10 +12,23 @@ def extern_request(url):
                 'follow_redirect': 'true', 'retry_404': 'true' }
     return requests.get(SCRAP_URL, params=PAYLOAD)
 
-@app.get("/{artist_name}/{song_name}")
+@app.get("/")
 async def root(artist_name,song_name):
-    print(unquote_plus(artist_name))
-    print(unquote_plus(song_name))
+    print(unquote(artist_name))
+    print(unquote(song_name))
+    lyrics = get_lyrics(
+                    artist_name = artist_name,
+                    song_name = song_name,
+                    #request = extern_request,
+                    #save_json = True,
+                    return_json = True
+                    )
+    return lyrics
+
+@app.get("/{artist_name}/{song_name}")
+async def get_lyrics_view(artist_name,song_name):
+    print(unquote(artist_name))
+    print(unquote(song_name))
     lyrics = get_lyrics(
                     artist_name = artist_name,
                     song_name = song_name,
