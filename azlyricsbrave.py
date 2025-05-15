1f171cb5ad40f05ba0f71822a754488e9d923b3e
sebacris97 <counterlaplata@live.com>
2025-05-14 21:29:23 -0300

    exploring other lyrics providers

--- azlyricsbrave.py
+++ azlyricsbrave.py
@@ -1,16 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 import os
-from fake_useragent import UserAgent
 import re
 
-SEARCH_SITE = "azlyrics.com"
+#SEARCH_SITE = "azlyrics.com"
+SEARCH_SITE = "lyrics.com"
 BRAVE_URL = 'https://api.search.brave.com/res/v1/web/search?q='
 
-UA = UserAgent()
 
 DEFAULT_HEADERS = {
-                  'User-Agent':UA.random,
+                  'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36',
                   'Accept-Encoding': 'gzip, deflate',
                   'Accept': '*/*',
                   'Connection': 'keep-alive'
@@ -40,13 +39,13 @@ def brave_search(artist, song, request=default_request):
 
 def scraped_song_lyrics(response):
     soup = BeautifulSoup(response.text,'html.parser')
-    return soup.find_all("div")[24].text.replace('\r','')
-
+    #return soup.find_all("div")[24].text.replace('\r','')
+    return soup.find(id="lyric-body-text").text
 
 def clean_data(response):
-    url = response['web']['results'][0]['url']
     artist = response['web']['results'][0]['title'].split(' - ')[0]
     song = response['web']['results'][0]['title'].split(' - ')[1].split(' Lyrics')[0]
+    url = response['web']['results'][0]['url']
     return artist, song, url
 
 
@@ -56,4 +55,5 @@ def get_brave_lyrics(artist='',song='',request=default_request):
     lyrics_response = request(lyrics_url)
     lyrics = scraped_song_lyrics(lyrics_response)
     return {'artist':artist,'song':song,'lyrics':lyrics}
+
 
